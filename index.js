88626a47cdbba45c878871aec6437e684f8a06a3
lokesh0127 <111865373+lokesh0127@users.noreply.github.com>
2023-11-22 14:07:35 +0530

    question 4 done

--- index.js
+++ index.js
@@ -13,3 +13,10 @@ header.style.border = "solid 3px black"
 let containerTitle = document.getElementsByClassName("title")
 containerTitle[0].style.fontWeight = "bold"
 containerTitle[0].style.color = "green"
+
+let items = document.getElementsByClassName("list-group-item")
+items[2].style.backgroundColor = "green"
+
+for(let i=0; i<items.length; i++){
+    items[i].style.fontWeight = "bold "
+}
