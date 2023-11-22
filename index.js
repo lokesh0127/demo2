b83d3df66788a6d82b5577b6b71e3e19ad24eac2
lokesh0127 <111865373+lokesh0127@users.noreply.github.com>
2023-11-22 16:54:53 +0530

    Question 6 done

--- index.js
+++ index.js
@@ -27,3 +27,19 @@ for(let i=0; i<items.length; i++){
 
 let list = document.getElementsByTagName("li")
 list[4].textContent = "Hello world"
+
+let secondItem = document.querySelector(".list-group-item:nth-child(2)")
+secondItem.style.backgroundColor = "green"
+
+let thirdItem = document.querySelector(".list-group-item:nth-child(3)")
+thirdItem.style.display =  "none"
+
+let list = document.querySelectorAll("li")
+list[1].style.color = "green"
+
+
+let odd = document.querySelectorAll("li:nth-child(odd)")
+
+for(let i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor = "green"
+}
