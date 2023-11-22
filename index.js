887ee0174f2b1f85e0e8ba4090cbd65d23752d1e
lokesh0127 <111865373+lokesh0127@users.noreply.github.com>
2023-11-22 12:58:14 +0530

    tasks submitted

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("this is my first file to add");
+console.dir(document);
+
+console.log(document.head);
+console.log(document.body);
+console.log(document.URL);
+console.log(document.forms);
+document.title = "Items"
+
+let header = document.getElementById("main-header")
+header.style.border = "solid 3px black"
+
+let containerTitle = document.getElementsByClassName("title")
+containerTitle[0].style.fontWeight = "bold"
+containerTitle[0].style.color = "green"
