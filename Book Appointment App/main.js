c2d4a26941e5cdcbe0c5c97dc0976a12c5bd2760
lokesh0127 <111865373+lokesh0127@users.noreply.github.com>
2023-11-23 21:26:55 +0530

    Question 10 done

---
+++ Book Appointment App/main.js
@@ -0,0 +1,28 @@
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  
+  if(nameInput.value === '' || emailInput.value === '') {
+ 
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter all fields';
+
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+
+    localStorage.setItem("Name" , nameInput.value)
+    localStorage.setItem("Email" , emailInput.value)
+
+    nameInput.value = '';
+    emailInput.value = '';
+  }
+}
