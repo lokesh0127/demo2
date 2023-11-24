ae7d68e385b509af5290d12bcefc98bc7c892814
lokesh0127 <111865373+lokesh0127@users.noreply.github.com>
2023-11-24 11:08:48 +0530

    Question 11 done

--- Book Appointment App/main.js
+++ Book Appointment App/main.js
@@ -6,23 +6,40 @@ const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
 
 
-myForm.addEventListener('submit', onSubmit);
+// myForm.addEventListener('submit', onSubmit);
 
-function onSubmit(e) {
-  e.preventDefault();
+// function onSubmit(e) {
+//   e.preventDefault();
   
-  if(nameInput.value === '' || emailInput.value === '') {
+//   if(nameInput.value === '' || emailInput.value === '') {
  
-    msg.classList.add('error');
-    msg.innerHTML = 'Please enter all fields';
+//     msg.classList.add('error');
+//     msg.innerHTML = 'Please enter all fields';
 
-    setTimeout(() => msg.remove(), 3000);
-  } else {
+//     setTimeout(() => msg.remove(), 3000);
+//   } else {
 
-    localStorage.setItem("Name" , nameInput.value)
-    localStorage.setItem("Email" , emailInput.value)
+//     localStorage.setItem("Name" , nameInput.value)
+//     localStorage.setItem("Email" , emailInput.value)
 
-    nameInput.value = '';
-    emailInput.value = '';
-  }
-}
+//     nameInput.value = '';
+//     emailInput.value = '';
+//   }
+// }
+
+
+myForm.addEventListener("submit", function (e) {
+  e.preventDefault(); 
+
+  var existingUserDetails = JSON.parse(localStorage.getItem("userDetails")) || [];
+  
+  var userDetails = {
+    username: nameInput.value,
+    email: emailInput.value,
+  };
+
+  existingUserDetails.push(userDetails);
+  
+  localStorage.setItem("userDetails", JSON.stringify(existingUserDetails));
+ 
+});
